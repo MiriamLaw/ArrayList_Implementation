903581901dd406a6461ef19a4d012a795eb18ee5
MiriamLaw <mimsical999@yahoo.com>
2023-09-20 20:50:32 -0700

    Refactored Final Commit post-code review

--- src/com/coderscampus/arraylist/CustomArrayList.java
+++ src/com/coderscampus/arraylist/CustomArrayList.java
@@ -1,41 +1,41 @@
 package com.coderscampus.arraylist;
 
+import java.util.Arrays;
+
 public class CustomArrayList<T> implements CustomList<T> {
 	private Object[] items = new Object[10];
-	private int elementCount = 0;
+	private int size = 0;
 
 	@Override
 	public boolean add(T item) {
 
-		if (elementCount == items.length) {
-			Object[] newItems = new Object[items.length * 2];
-
-			System.arraycopy(items, 0, newItems, 0, elementCount);
-
-			items = newItems;
-		}
-
-		items[elementCount] = item;
-
-		elementCount++;
-
+		increaseArraySizeIfNeeded();
+		items[size] = item;
+		size++;
 		return true;
 	}
 
 	@Override
 	public int getSize() {
-		return elementCount;
+		return size;
 	}
 
 	@Override
 	public T get(int index) {
-		if (index < 0 || index >= elementCount) {
+		if (index < 0 || index >= size) {
 			throw new IndexOutOfBoundsException("Index is out of bounds");
 
 		}
 		@SuppressWarnings("unchecked")
 		T element = (T) items[index];
 		return element;
+	}
+
+	private void increaseArraySizeIfNeeded() {
+		if (size == items.length) {
+			Object[] newItems = Arrays.copyOf(items, items.length * 2);
+			items = newItems;
+		}
 	}
 
 }
--- src/com/coderscampus/arraylist/CustomListApplication.java
+++ src/com/coderscampus/arraylist/CustomListApplication.java
@@ -4,27 +4,23 @@ public class CustomListApplication {
 
 	public static void main(String[] args) {
 
-		CustomList<String> futbolistas = new CustomArrayList<>();
-
-		futbolistas.add("Rapinoe");
-		futbolistas.add("Putellas");
-		futbolistas.add("Kerr");
-		futbolistas.add("Morgan");
-		futbolistas.add("McCabe");
-		futbolistas.add("Miedema");
-		futbolistas.add("Hermoso");
-		futbolistas.add("Bonmati");
-		futbolistas.add("Ertz");
-		futbolistas.add("O'Hara");
-		futbolistas.add("Naeher");
-		futbolistas.add("Dunn");
-		futbolistas.add("Carmona");
-		futbolistas.add("Coll");
-		futbolistas.add("Paredes");
-
-		for (int i = 0; i < futbolistas.getSize(); i++) {
-			System.out.println(futbolistas.get(i));
+		CustomList<Integer> futbolistas = new CustomArrayList<>();
+		
+		for (int i = 0; i < 51; i++) {
+			futbolistas.add(i);
+			System.out.println(futbolistas.getSize());
 		}
+		System.out.println("-------");
+		System.out.println(futbolistas.get(44));
+		System.out.println(futbolistas.get(7));
+		System.out.println(futbolistas.get(41));
+		System.out.println(futbolistas.get(33));
+
+
+
+//		for (int i = 0; i < futbolistas.getSize(); i++) {
+//			System.out.println(futbolistas.get(i));
+//		}
 	}
 
 }
