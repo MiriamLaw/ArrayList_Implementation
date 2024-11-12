8f1fba51c91cb36a2474b4b0bea4fb51bd39a53f
MiriamLaw <mimsical999@yahoo.com>
2024-11-12 17:16:53 -0600

    Update CustomArrayList.java

--- src/com/coderscampus/arraylist/CustomArrayList.java
+++ src/com/coderscampus/arraylist/CustomArrayList.java
@@ -15,6 +15,21 @@ public class CustomArrayList<T> implements CustomList<T> {
 		return true;
 	}
 
+	public boolean addAtIndex(int index, T item) {
+		if (index < 0 || index > size) {
+			throw new IndexOutOfBoundsException("Index is out of bounds");
+		}
+
+		increaseArraySizeIfNeeded(); // Ensure there is enough space
+
+		// Shift elements to the right to make space for the new item
+		System.arraycopy(items, index, items, index + 1, size - index);
+
+		items[index] = item; // Insert the new item
+		size++; // Increment the size
+		return true;
+	}
+
 	@Override
 	public int getSize() {
 		return size;
@@ -29,6 +44,26 @@ public class CustomArrayList<T> implements CustomList<T> {
 		@SuppressWarnings("unchecked")
 		T element = (T) items[index];
 		return element;
+	}
+
+	public T remove(int index) {
+		if (index < 0 || index >= size) {
+			throw new IndexOutOfBoundsException("Index is out of bounds");
+		}
+
+		// Store the element to be removed
+		T removedElement = get(index);
+
+		// Shift elements to the left to fill the gap
+		System.arraycopy(items, index + 1, items, index, size - index - 1);
+
+		// Nullify the last element to avoid duplication
+		items[size - 1] = null;
+
+		// Decrease the size
+		size--;
+
+		return removedElement; // Return the removed element
 	}
 
 	private void increaseArraySizeIfNeeded() {
